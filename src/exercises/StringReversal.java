7d3eb90ecddd66f74d54bf21fa8f15a7463dd0a8
a.nesic <razor9514@gmail.com>
2019-05-31 16:26:20 +0200

    Second Change

--- src/exercises/StringReversal.java
+++ src/exercises/StringReversal.java
@@ -11,8 +11,22 @@ public class StringReversal extends TestCase {
 	 *       QcXgW9w4wQd=v?hctaw/moc.ebutuoy
 	 */
 
-//	public void test() {
-//		assertEquals("cellardoor", reverseMe("roodrallec"));
-//	}
+	public void test() {
+		assertEquals("cellardoor", reverseMe("roodrallec"));
+	System.out.println(reverseMe("QcXgW9w4wQd=v?hctaw/moc.ebutuoy"));
+	}
+
+	private String reverseMe(String string) {
+		String reverse = "";
+		for (int i =string.length()-1; i>=0; i--) {
+			reverse = reverse + string.charAt(i);}
+		return reverse;
+			
+		}
+	
+
 
 }
+	
+
+
